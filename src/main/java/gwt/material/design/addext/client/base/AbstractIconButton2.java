748836a09ebebc2c0aeb4d8f901a6879999ae836
CloudRobi <csernikr@hotmail.com>
2019-01-17 16:10:33 +0100

    v2.2 kibocsátás

--- src/main/java/gwt/material/design/addext/client/base/AbstractIconButton2.java
+++ src/main/java/gwt/material/design/addext/client/base/AbstractIconButton2.java
@@ -40,125 +40,138 @@ import gwt.material.design.client.ui.MaterialIcon;
  */
 public abstract class AbstractIconButton2 extends AbstractButton implements HasIcon {
 
-    private MaterialIcon icon = new MaterialIcon();
-    private Label label = new Label();
-    
-    public AbstractIconButton2(ButtonType type, String text, MaterialIcon icon) {
-        super(type, text);
+	private MaterialIcon icon = new MaterialIcon();
+	private Label label = new Label();
 
-        this.icon = icon;
-        ensureIconAttached();
-    }
+	public AbstractIconButton2(ButtonType type, String text, MaterialIcon icon) {
+		super(type, text);
 
-    public AbstractIconButton2(ButtonType type, String text) {
-        super(type, text);
-        setIconPosition(IconPosition.LEFT);
-    }
+		this.icon = icon;
+		ensureIconAttached();
+	}
 
-    public AbstractIconButton2(IconType iconType) {
-        super();
-        setIconType(iconType);
-        setIconPosition(IconPosition.LEFT);
-    }
+	public AbstractIconButton2(ButtonType type, String text) {
+		super(type, text);
+		setIconPosition(IconPosition.LEFT);
+	}
 
-    public AbstractIconButton2(ButtonType type) {
-        super(type);
-        setIconPosition(IconPosition.LEFT);
-    }
+	public AbstractIconButton2(IconType iconType) {
+		super();
+		setIconType(iconType);
+		setIconPosition(IconPosition.LEFT);
+	}
 
-    public AbstractIconButton2() {
-        super();
-        setIconPosition(IconPosition.LEFT);
-    }
+	public AbstractIconButton2(ButtonType type) {
+		super(type);
+		setIconPosition(IconPosition.LEFT);
+	}
 
-    public AbstractIconButton2(String... initialClass) {
-        super();
-        setInitialClasses(initialClass);
-    }
+	public AbstractIconButton2() {
+		super();
+		setIconPosition(IconPosition.LEFT);
+	}
 
-    @Override
-    public MaterialIcon getIcon() {
-        return icon;
-    }
+	public AbstractIconButton2(String... initialClass) {
+		super();
+		setInitialClasses(initialClass);
+	}
 
-    @Override
-    public void setIconType(IconType iconType) {
-        icon.setIconType(iconType);
-        ensureIconAttached();
-    }
+	@Override
+	public MaterialIcon getIcon() {
+		return icon;
+	}
 
-    @Override
-    public void setIconPosition(IconPosition position) {
-        icon.setIconPosition(position);
-    }
+	@Override
+	public void setIconType(IconType iconType) {
+		icon.setIconType(iconType);
+		ensureIconAttached();
+	}
 
-    @Override
-    public void setIconSize(IconSize size) {
-        icon.setIconSize(size);
-    }
+	@Override
+	public void setIconPosition(IconPosition position) {
+		icon.setIconPosition(position);
+	}
 
-    @Override
-    public void setIconFontSize(double size, Style.Unit unit) {
-        icon.setIconFontSize(size, unit);
-    }
+	@Override
+	public void setIconSize(IconSize size) {
+		icon.setIconSize(size);
+	}
 
-    @Override
-    public void setIconColor(Color iconColor) {
-        icon.setIconColor(iconColor);
-    }
+	@Override
+	public void setIconFontSize(double size, Style.Unit unit) {
+		icon.setIconFontSize(size, unit);
+	}
 
-    @Override
-    public Color getIconColor() {
-        return icon.getIconColor();
-    }
+	@Override
+	public void setIconColor(Color iconColor) {
+		icon.setIconColor(iconColor);
+	}
 
-    @Override
-    public void setIconPrefix(boolean prefix) {
-        icon.setIconPrefix(prefix);
-    }
+	@Override
+	public Color getIconColor() {
+		return icon.getIconColor();
+	}
 
-    @Override
-    public boolean isIconPrefix() {
-        return icon.isIconPrefix();
-    }
+	@Override
+	public void setIconPrefix(boolean prefix) {
+		icon.setIconPrefix(prefix);
+	}
 
-    /**
-     * Ensure the icon is attached in slot 0.
-     */
-    protected void ensureIconAttached() {
-        if (icon != null && !icon.isAttached()) {
-            insert(icon, 0);
-        }
-    }
-    
-    /**
-     * Get the buttons span text.
-     */
-    public String getText() {
-        return label.getText();
-    }
+	@Override
+	public boolean isIconPrefix() {
+		return icon.isIconPrefix();
+	}
 
-    /**
-     * Set the buttons span text.
-     */
-    public void setText(String text) {
-    	label.setText(text);
+	/**
+	 * Ensure the icon is attached in slot 0.
+	 */
+	protected void ensureIconAttached() {
+		if (icon != null && !icon.isAttached()) {
+			insert(icon, 0);
+		}
+	}
 
-        if (!label.isAttached()) {
-            add(label);
-        }
-}
-    /**
-     * Get the buttons span text.
-     */
-    public String getInnerText() {
-        return super.getText();
-    }
+	/**
+	 * Get the buttons span text.
+	 */
+	public String getText() {
+		return label.getText();
+	}
 
-    /**
-     * Set the buttons span text.
-     */
-    public void setInnerText(String text) {
-        super.setText(text);
-}
+	/**
+	 * Set the buttons span text.
+	 */
+	public void setText(String text) {
+		label.setText(text);
+
+		if (!label.isAttached()) {
+			add(label);
+		}
+	}
+
+	/**
+	 * Get the buttons span text.
+	 */
+	public String getInnerText() {
+		return super.getText();
+	}
+
+	/**
+	 * Set the buttons span text.
+	 */
+	public void setInnerText(String text) {
+		super.setText(text);
+	}
+
+	@Override
+	public void setCustomIconType(String iconType) {
+		icon.setCustomIconType(iconType);
+		ensureIconAttached();
+	}
+
+	@Override
+	public String getCustomIconType() {
+		return icon.getCustomIconType();
+	}
+
 }
